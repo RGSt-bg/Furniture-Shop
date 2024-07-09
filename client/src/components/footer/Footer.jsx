c94da889a77e51fae6e520a2e4748ad8e519f737
Rumen Stefanov <rgst.bg@gmail.com>
2024-07-09 11:33:13 +0300

    Maked Header and Footer components

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div id="footer">
+      <p>
+        <a href="index.html">Home</a> | <a href="furnitures.html">Furnitures</a> | <a href="furnitureList.html">New Products</a> | <a href="about.html">About Us</a> | <a href="contact.html">Contact Us</a>
+      </p>
+      <p>
+        Copyright &copy; simplestyle_3 | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">HTML5 Web Templates</a>
+      </p>
+    </div>
+  );
+}
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,51 @@
+export default function Header() {
+  return (
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <h1>
+            <a href="index.html">
+              Furniture <span className="logo_colour">Shop</span>
+            </a>
+          </h1>
+          <h2>Simple. Comfortable. At affordable prices.</h2>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          {/* <!-- put className="selected" in the "li" tag for the selected page - to highlight which page you're on --> */}
+          <li className="selected">
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="furnitures.html">Furnitures</a>
+          </li>
+          <li>
+            <a href="furnitureList.html">New Products</a>
+          </li>
+          <li>
+            <a href="editCreate.html">Create Furniture</a>
+          </li>
+          <li>
+            <a href="createCategory.html">Create Category</a>
+          </li>
+          <li>
+            <a href="contact.html">Contacts</a>
+          </li>
+          <li>
+            <a href="about.html">About</a>
+          </li>
+          <li>
+            <a href="register.html">Register</a>
+          </li>
+          <li>
+            <a href="login.html">Login</a>
+          </li>
+          <li>
+            <a href="login.html">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
