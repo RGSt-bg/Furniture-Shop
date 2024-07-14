75bcfd1807407afaf6e74dc998de46afc4d5d23c
Rumen Stefanov <rgst.bg@gmail.com>
2024-07-14 18:06:35 +0300

    Added routing to the app

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,16 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import Sidebar from "./sidebar/Sidebar.jsx";
 import SiteContent from "./site-content/SiteContent.jsx";
 import FurnitureList from "./site-content/furniture-list/FurnitureList.jsx";
 import Categories from "./site-content/categories/Categories.jsx";
+import About from "./site-content/about/About.jsx";
+import Contacts from "./site-content/contacts/Contacts.jsx";
 
 export default function Main() {
    return(
     <div id="main">
       <div id="site_content">
         <Sidebar />
-        <SiteContent />
-        <FurnitureList />
-        <Categories />
+        <Routes>
+            <Route path="/" element={<SiteContent />} />
+            <Route path="/furniture/categories" element={<Categories />} />
+            <Route path="/furniture/furnitures" element={<FurnitureList />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+        </Routes>
       </div>
     </div>
    );
