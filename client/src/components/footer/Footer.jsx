e345289d0317b538e29ce869b57636dbd68c8542
Rumen Stefanov <rgst.bg@gmail.com>
2024-07-14 18:04:35 +0300

    Added Link attribute

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <div id="footer">
       <p>
-        <a href="index.html">Home</a> | <a href="furnitures.html">Furnitures</a> | <a href="furnitureList.html">New Products</a> | <a href="about.html">About Us</a> | <a href="contact.html">Contact Us</a>
+        <Link to="/">Home</Link> | <Link to="/furniture/categories">Furnitures</Link> | <Link to="/furniture/furnitures">New Products</Link> | <Link to="/about">About Us</Link> | <Link to="/contact">Contact Us</Link>
       </p>
       <p>
-        Copyright &copy; simplestyle_3 | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">HTML5 Web Templates</a>
+        Copyright &copy; simplestyle_3 | <Link to="http://validator.w3.org/check?uri=referer">HTML5</Link> | <Link to="http://jigsaw.w3.org/css-validator/check/referer">CSS</Link> | <Link to="http://www.html5webtemplates.co.uk">HTML5 Web Templates</Link>
       </p>
     </div>
   );
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,49 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <div id="header">
       <div id="logo">
         <div id="logo_text">
           <h1>
-            <a href="index.html">
-              Furniture <span className="logo_colour">Shop</span>
-            </a>
+          <Link to="/">Fu<span className="logo_colour">rni</span>ture <span className="logo_colour">Shop</span></Link>
           </h1>
           <h2>Simple. Comfortable. At affordable prices.</h2>
         </div>
       </div>
       <div id="menubar">
         <ul id="menu">
-          {/* <!-- put class/className="selected" in the "li" tag for the selected page - to highlight which page you're on --> */}
-          <li className="selected">
-            <a href="index.html">Home</a>
-          </li>
-          <li>
-            <a href="furnitures.html">Furnitures</a>
-          </li>
-          <li>
-            <a href="furnitureList.html">New Products</a>
-          </li>
-          <li>
-            <a href="editCreate.html">Create Furniture</a>
-          </li>
-          <li>
-            <a href="createCategory.html">Create Category</a>
-          </li>
-          <li>
-            <a href="contact.html">Contacts</a>
-          </li>
-          <li>
-            <a href="about.html">About</a>
-          </li>
-          <li>
-            <a href="register.html">Register</a>
-          </li>
-          <li>
-            <a href="login.html">Login</a>
-          </li>
-          <li>
-            <a href="login.html">Logout</a>
-          </li>
+          <li className="selected"><Link to="/">Home</Link></li>
+          <li><Link to="/furniture/categories">Furnitures</Link></li>
+          <li><Link to="/furniture/furnitures">New Products</Link></li>
+          <li><Link to="editCreate.html">Create Furniture</Link></li>
+          <li><Link to="createCategory.html">Create Category</Link></li>
+          <li><Link to="/contacts">Contacts</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/auth/register">Register</Link></li>
+          <li><Link to="/auth/login">Login</Link></li>
+          <li><Link to="/auth/login">Logout</Link></li>
         </ul>
       </div>
     </div>
