957b07bca96f2de9fb1d4503778b5cecf5e181c5
Rumen Stefanov <rgst.bg@gmail.com>
2024-07-27 17:45:49 +0300

    Update CreateCategory component

--- client/src/components/main/site-content/createCategory/CreateCategory.jsx
+++ client/src/components/main/site-content/createCategory/CreateCategory.jsx
@@ -5,13 +5,18 @@ export default function CreateCategory() {
     <form action="" method="post">
       <div className="form_settings">
         <p>
-          <span>Category</span>
-          <input className="contact" type="text" name="category" defaultValue="" />
+          <span>Category*</span>
+          <input className="contact" type="text" name="category" required minLength="3"defaultValue="" />
+        </p>
+        <p>
+          <span>Image*</span>
+          <input className="contact" type="text" name="imageCategory" required placeholder="Valid image url ..." defaultValue="" />
         </p>
         <p className="createCategory">
           <span>&nbsp;</span>
-          <input className="submit" type="submit" name="contact_submitted" value="Create Category" />
+          <input className="submit" type="submit" name="contact_submitted" value="Create" />
         </p>
+        <p>* - required field</p>
       </div>
     </form>
   </div>
