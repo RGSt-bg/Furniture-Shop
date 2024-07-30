7941a526283b8b7d68d23c4695b93576e1f7d9e3
Rumen Stefanov <rgst.bg@gmail.com>
2024-07-30 11:10:00 +0300

    Made error handling and user notices in CreateCategory component

--- client/src/components/main/site-content/createCategory/CreateCategory.jsx
+++ client/src/components/main/site-content/createCategory/CreateCategory.jsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
+
+import { createCategory } from "../../../../utils/furnitureUtils.js";
+
 export default function CreateCategory() {
+
+  const [formValues, setFormValues] = useState({
+    category: "",
+    imageCategory: "",
+  });
+
+  const [message, setMessage] = useState("");
+  
+  const formValuesHandler = (e) => {
+    e.preventDefault();
+    setFormValues(oldValues => ({ ...oldValues, [e.target.name]: e.target.value }));
+  };
+  
+  const formSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await createCategory("/furniture/createCategory", formValues);
+      setMessage(response.message);
+      alert(response.message);
+    } catch (err) {
+      setMessage("An error occurred while creating the category.");
+      alert(message);
+    };
+  };
+  
    return(
     <div id="content">
     <h1><b><i>Create Category</i></b></h1>
-    <form action="" method="post">
+    <form onSubmit={formSubmitHandler}>
       <div className="form_settings">
         <p>
           <span>Category*</span>
-          <input className="contact" type="text" name="category" required minLength="3"defaultValue="" />
+          <input className="contact" type="text" name="category" required minLength="3" value={formValues.category} onChange={formValuesHandler} />
         </p>
         <p>
           <span>Image*</span>
-          <input className="contact" type="text" name="imageCategory" required placeholder="Valid image url ..." defaultValue="" />
+          <input className="contact" type="text" name="imageCategory" required placeholder="Valid image url ..." value={formValues.imageCategory} onChange={formValuesHandler}/>
         </p>
         <p className="createCategory">
           <span>&nbsp;</span>
