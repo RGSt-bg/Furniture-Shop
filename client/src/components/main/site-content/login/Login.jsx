e81a97fcfa930850b54d88bd3a402c9ac38fd781
Rumen Stefanov <rgst.bg@gmail.com>
2024-07-31 12:01:37 +0300

    Made Login component

--- client/src/components/main/site-content/login/Login.jsx
+++ client/src/components/main/site-content/login/Login.jsx
@@ -1,18 +1,48 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { login } from "../../../../utils/authUtils.js";
+
 export default function Login() {
+  
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [message, setMessage] = useState("");
+  
+  const formValuesHandler = (e) => {
+    e.preventDefault();
+    setFormValues(oldValues => ({ ...oldValues, [e.target.name]: e.target.value }));
+  };
+    
+  const formSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await login("/auth/login", formValues);
+      setMessage(response.message);
+      alert(response.message);
+      setFormValues({ email: "", password: "" });
+    } catch (err) {
+      setMessage("An error occurred while logging!");
+      alert(message);
+    };
+  };
+  
    return(
     <div id="content">
     <h1><b><i>Login Form</i></b></h1>
-    <form action="./index.html" method="post">
+    <form onSubmit={formSubmitHandler}>
       <div className="form_settings">
         <p>
           <span>Email address</span>
-          <input className="contact" type="text" name="your_email" value="" />
+          <input className="contact" type="email" name="email" required value={formValues.email} onChange={formValuesHandler} />
         </p>
         <p>
           <span>Password</span>
-          <input className="contact" type="password" name="password" value="" />
+          <input className="contact" type="password" name="password" required value={formValues.password} onChange={formValuesHandler} />
         </p>
         <p className="login">
           <span>&nbsp;</span>
---
+++ client/src/utils/authUtils.js
@@ -0,0 +1,15 @@
+import { BASE_URL } from "./variables.js";
+
+export const login = async function (path, loginData) {
+    try {
+        const response = await fetch(`${BASE_URL}${path}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json", },
+            body: JSON.stringify(loginData),
+            });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        alert(error.message);
+    }
+}
