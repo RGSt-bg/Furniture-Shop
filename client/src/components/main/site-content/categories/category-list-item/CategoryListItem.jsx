7f733f58f6e9c4f83661a85fac1935e8d2d2c929
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-01 14:53:27 +0300

    Update class in CategoryListItem layout

--- client/src/components/main/site-content/categories/category-list-item/CategoryListItem.jsx
+++ client/src/components/main/site-content/categories/category-list-item/CategoryListItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function CategoryListItem(props) {
     const category = props.category;
     return(
-    <div className={category.location}>
+    <div className={`categories ${category.location}`}>
         <Link className="category" to="/furniture/categories"><img src={category.imageCategory} alt={category.category} /></Link>
         <Link className="category" to="/furniture/categories"><h2>{category.category}</h2></Link>
     </div>
