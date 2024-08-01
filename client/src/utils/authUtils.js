444b0d43958a9e63dcdf3f46352dfdb9a76ed1eb
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-01 14:50:26 +0300

    Updated Register, Login and Logout functions

--- client/src/utils/authUtils.js
+++ client/src/utils/authUtils.js
@@ -1,19 +1,5 @@
 import { BASE_URL } from "./variables.js";
 
-export const login = async function (path, loginData) {
-    try {
-        const response = await fetch(`${BASE_URL}${path}`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json", },
-            body: JSON.stringify(loginData),
-            });
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        alert(error.message);
-    }
-};
-
 export const register = async function (path, registerData) {
     try {
         const response = await fetch(`${BASE_URL}${path}`, {
@@ -22,8 +8,38 @@ export const register = async function (path, registerData) {
             body: JSON.stringify(registerData),
             });
         const data = await response.json();
+        localStorage.setItem("auth", data.token);
         return data;
     } catch (error) {
         alert(error.message);
     }
 }
+
+export const login = async function (path, loginData) {
+    try {
+        const response = await fetch(`${BASE_URL}${path}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json", },
+            body: JSON.stringify(loginData),
+            });
+        const data = await response.json();
+        localStorage.setItem("auth", data.token);
+        return data;
+    } catch (error) {
+        alert(error.message);
+    }
+};
+
+export const logout = async function (path) {
+    try {
+        const response = await fetch(`${BASE_URL}${path}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json", },
+            });
+        const data = await response.json();
+        localStorage.removeItem("auth");
+        return data;
+    } catch (error) {
+        alert(error.message);
+    }
+};
