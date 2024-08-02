eff39ab37c6cc723b467584e9eca2c0c96adb0bb
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-02 14:29:45 +0300

    Added AuthContext to whole application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
+import { useState, createContext } from "react";
+
 import Header from './components/header/Header.jsx';
 import Main from './components/main/Main.jsx';
 import Footer from './components/footer/Footer.jsx';
 import './styles/site.css';
 
+export const AuthContext = createContext(false);
+
 function App() {
+
+  const [isAuth, setIsAuth] = useState(false);
 
   return (
     <>
-      <Header />
-
-      <Main />
-
-      <Footer />
+      <AuthContext.Provider value={{isAuth, setIsAuth}}>
+        <Header />
+        <Main />
+        <Footer />
+      </AuthContext.Provider>
     </>
   );
 };
