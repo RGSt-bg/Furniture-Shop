35dec42ce987ac398ff826d08464829349611a48
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-04 20:28:16 +0300

    Changed the text of placeholder of search field

--- client/src/components/main/sidebar/Sidebar.jsx
+++ client/src/components/main/sidebar/Sidebar.jsx
@@ -11,7 +11,7 @@ export default function Sidebar() {
                 className="search"
                 type="text"
                 name="search_field"
-                placeholder="Enter keywords....."
+                placeholder="Enter part of keyword..."
               />
               <input
                 name="search"
