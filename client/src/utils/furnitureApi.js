bc9aa6a17796a539927a1ff383727131ec2e2aca
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-05 23:50:40 +0300

    Separated API and furniture utils

---
+++ client/src/utils/furnitureApi.js
@@ -0,0 +1,88 @@
+import { BASE_URL } from "./variables.js";
+
+export const getFurnitures = async function (path, getFurnitures) {
+  try {
+      const response = await fetch(`${BASE_URL}${path}`);
+      const data = await response.json();
+      const furnitures = Object.values(data);
+
+      return getFurnitures(furnitures);
+  } catch (error) {
+      alert(error.message);
+  }
+};
+
+export const getFurnitureDetails = async function (path, setFurnitureDetails) {
+
+  try {
+      const response = await fetch(`${BASE_URL}${path}`);
+      const furnitureDetails = await response.json();
+
+      return setFurnitureDetails(furnitureDetails);
+  } catch (error) {
+      alert(error.message);
+  }
+};
+
+export const createCategory = async function (path, categoryData) {
+  try {
+      const response = await fetch(`${BASE_URL}${path}`, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(categoryData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response is not OK!");
+      }
+      const data = await response.json();
+      return data;
+      
+  } catch (error) {
+      alert(error.message);
+  }
+};
+
+export const createFurnitures = async function (path, furnitureData) {
+  try {
+      const response = await fetch(`${BASE_URL}${path}`, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(furnitureData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response is not OK!");
+      }
+      const data = await response.json();
+      return data;
+      
+  } catch (error) {
+      alert(error.message);
+  }
+};
+
+export const editFurnitures = async function (path, furnitureData) {
+  try {
+      const response = await fetch(`${BASE_URL}${path}`, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(furnitureData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response is not OK!");
+      }
+      const data = await response.json();
+      return data;
+      
+  } catch (error) {
+      alert(error.message);
+  }
+};
--- client/src/utils/furnitureUtils.js
+++ client/src/utils/furnitureUtils.js
@@ -1,9 +1,11 @@
-import { BASE_URL } from "./variables.js";
-
-export default function calledFromWhere(calledFrom, category) {
+export const calledFromWhere = function (calledFrom, category) {
   let isNewTitle = "";
   let isNew = "";
   let noFurnitures = "";
+  let path = `?calledFrom=${calledFrom}`;
+
+  if (category)
+    path = `${path}&category=${category}`;
 
   if (calledFrom === "newProducts") {
     isNew = "Here you can find our NEW furnitures - elegant, comfortable, functional ...";
@@ -23,101 +25,9 @@ export default function calledFromWhere(calledFrom, category) {
     isNewTitle = "Our Furnitures";
   }
 
-  return [isNew, noFurnitures, isNewTitle];
+  return [isNew, noFurnitures, isNewTitle, path];
 }
 
 // export default excludeCategory(categoryName, category) {
 //   return category.filter((furniture) => furniture.category != categoryName);
-// }
-
-export const getFurnitures = async function (path, getFurnitures) {
-  try {
-      const response = await fetch(`${BASE_URL}${path}`);
-      const data = await response.json();
-      const furnitures = Object.values(data);
-
-      return getFurnitures(furnitures);
-  } catch (error) {
-      alert(error.message);
-  }
-};
-
-export const getFurnitureDetails = async function (path, setFurnitureDetails) {
-
-  try {
-      const response = await fetch(`${BASE_URL}${path}`);
-      const furnitureDetails = await response.json();
-
-      return setFurnitureDetails(furnitureDetails);
-  } catch (error) {
-      alert(error.message);
-  }
-};
-
-export const createCategory = async function (path, categoryData) {
-  try {
-      const response = await fetch(`${BASE_URL}${path}`, {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(categoryData),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response is not OK!");
-      }
-      const data = await response.json();
-      return data;
-      
-  } catch (error) {
-      alert(error.message);
-  }
-};
-
-export const createFurnitures = async function (path, furnitureData) {
-  try {
-      const response = await fetch(`${BASE_URL}${path}`, {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(furnitureData),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response is not OK!");
-      }
-      const data = await response.json();
-      return data;
-      
-  } catch (error) {
-      alert(error.message);
-  }
-};
-
-export const editFurnitures = async function (path, furnitureData) {
-  try {
-      const response = await fetch(`${BASE_URL}${path}`, {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(furnitureData),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response is not OK!");
-      }
-      const data = await response.json();
-      return data;
-      
-  } catch (error) {
-      alert(error.message);
-  }
-};
-
-// module.exports = {
-  // calledFromWhere,
-  // excludeCategory,
 // }
