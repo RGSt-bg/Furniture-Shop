99219f6024aa4bc2598b6d8afd293d1e826fc401
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-05 18:12:56 +0300

    Opened calledFromWhere function to use

--- client/src/utils/furnitureUtils.js
+++ client/src/utils/furnitureUtils.js
@@ -1,30 +1,30 @@
 import { BASE_URL } from "./variables.js";
 
-// export default function calledFromWhere(calledFrom, category) {
-//   let isNewTitle = "";
-//   let isNew = "";
-//   let noFurnitures = "";
+export default function calledFromWhere(calledFrom, category) {
+  let isNewTitle = "";
+  let isNew = "";
+  let noFurnitures = "";
 
-//   if (calledFrom === "newProducts") {
-//     isNew = "Here you can find our NEW furnitures - elegant, comfortable, functional ...";
-//     noFurnitures = "Sorry, there are no new furnitures!";
-//     isNewTitle = "New Furnitures";
-//   } else if (calledFrom === "search") {
-//     isNew = "The furniture that meets your criteria ...";
-//     noFurnitures = "Sorry, there are no furnitures that meets your criteria!";
-//     isNewTitle = "Found Furnitures";
-//   } else if (calledFrom === "category") {
-//     isNew = "The furnitures by category ...";
-//     noFurnitures = "Sorry, there are no furnitures in this category yet!";
-//     isNewTitle = `${category}`;
-//   } else {
-//     isNew = "Here you can find our furnitures - elegant, comfortable, functional ...";
-//     noFurnitures = "Sorry, there are no furnitures!";
-//     isNewTitle = "Our Furnitures";
-//   }
+  if (calledFrom === "newProducts") {
+    isNew = "Here you can find our NEW furnitures - elegant, comfortable, functional ...";
+    noFurnitures = "Sorry, there are no new furnitures!";
+    isNewTitle = "New Furnitures";
+  } else if (calledFrom === "search") {
+    isNew = "The furniture that meets your criteria ...";
+    noFurnitures = "Sorry, there are no furnitures that meets your criteria!";
+    isNewTitle = "Found Furnitures";
+  } else if (calledFrom === "category") {
+    isNew = "The furnitures by category ...";
+    noFurnitures = "Sorry, there are no furnitures in this category yet!";
+    isNewTitle = `${category}`;
+  } else {
+    isNew = "Here you can find our furnitures - elegant, comfortable, functional ...";
+    noFurnitures = "Sorry, there are no furnitures!";
+    isNewTitle = "Our Furnitures";
+  }
 
-//   return [isNew, noFurnitures, isNewTitle];
-// }
+  return [isNew, noFurnitures, isNewTitle];
+}
 
 // export default excludeCategory(categoryName, category) {
 //   return category.filter((furniture) => furniture.category != categoryName);
