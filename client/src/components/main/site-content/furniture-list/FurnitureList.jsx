c9ea4392fd45179c2a0621e7d84b74a15f15928b
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-06 00:05:52 +0300

    Update FurnitureList.jsx

--- client/src/components/main/site-content/furniture-list/FurnitureList.jsx
+++ client/src/components/main/site-content/furniture-list/FurnitureList.jsx
@@ -1,27 +1,39 @@
 import { useEffect, useState, useContext } from "react";
+import { useParams, useLocation } from "react-router-dom";
 
 import { CalledFromContext } from "../../Main.jsx";
 
 import FurnitureListItem from "./furniture-list-item/FurnitureListItem.jsx";
-import { getFurnitures } from "../../../../utils/furnitureUtils.js";
+import { getFurnitures } from "../../../../utils/furnitureApi.js";
+import { calledFromWhere } from "../../../../utils/furnitureUtils.js";
 
 export default function FurnitureList() {
 
+    const { category } = useParams() || '';
+    const location = useLocation();
+    
+    const {calledFrom, setCalledFrom} = useContext(CalledFromContext);
     const [furnitures, setFurnitures] = useState([]);
-
-    const {calledFrom} = useContext(CalledFromContext);
-console.log(calledFrom);
+    const [isNew, noFurnitures, isNewTitle, path] = calledFromWhere(calledFrom, category);
 
     useEffect(() => {
-        getFurnitures('/furniture/furnitureList', setFurnitures);
-    }, []);
+      const params = new URLSearchParams(location.search);
+      const calledFromQueryParam = params.get('calledFrom') || '';
+      if (calledFromQueryParam) {
+        setCalledFrom(calledFromQueryParam);
+      }
+    }, [location.search, setCalledFrom]);
+
+    useEffect(() => {
+        getFurnitures(`/furniture/furnitureList${path}`, setFurnitures);
+    }, [path]);
 
     return(
     <div id="content">
         <h1>
-          <b><i>New Products</i></b>
+          <b><i>{isNewTitle}</i></b>
         </h1>
-        <p>Here you can find our latest products - elegant, comfortable, functional ...</p>
+        <p>{isNew}</p>
 
         <section id="newProducts">
           <ul className="card-wrapper">
@@ -29,7 +41,7 @@ console.log(calledFrom);
           </ul>
 
         <div id="no-post">
-            {!furnitures.length && <h2>Sorry, there are no new Furnitures!</h2>}
+            {!furnitures.length && <h2>{noFurnitures}</h2>}
         </div>
         
         </section>
