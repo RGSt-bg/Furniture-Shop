58f400c9c48e10f3eda30a764f5d957ae55b6abe
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-06 00:03:19 +0300

    Added new link for calledFrom parameter

--- client/src/components/main/site-content/categories/category-list-item/CategoryListItem.jsx
+++ client/src/components/main/site-content/categories/category-list-item/CategoryListItem.jsx
@@ -14,8 +14,8 @@ export default function CategoryListItem(props) {
 
     return(
     <div className={`categories ${category.location}`}>
-        <Link className="category" to="/furniture/furnitures"><img src={category.imageCategory} alt={category.category} /></Link>
-        <Link className="category" to={`/furniture/categories?calledFrom=category&category=${category.category}`}><h2>{category.category}</h2></Link>
+        <Link className="category" to={`/furniture/furnitures/${category.category}`}><img src={category.imageCategory} alt={category.category} /></Link>
+        <Link className="category" to={`/furniture/furnitures/${category.category}`}><h2>{category.category}</h2></Link>
     </div>
    );
 };
