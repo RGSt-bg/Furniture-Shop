54673d70aaf1065ccefbadab47bffefd5a61459e
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-06 08:34:03 +0300

    Updated link to New Products

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div id="footer">
       <p>
-        <Link to="/">Home</Link> | <Link to="/furniture/categories">Furnitures</Link> | <Link to="/furniture/furnitures">New Products</Link> | <Link to="/about">About Us</Link> | <Link to="/contacts">Contact Us</Link>
+        <Link to="/">Home</Link> | <Link to="/furniture/categories">Furnitures</Link> | <Link to="/furniture/furnitures?calledFrom=newProducts">New Products</Link> | <Link to="/about">About Us</Link> | <Link to="/contacts">Contact Us</Link>
       </p>
       <p>
         Copyright &copy; simplestyle_3 | <Link to="http://validator.w3.org/check?uri=referer">HTML5</Link> | <Link to="http://jigsaw.w3.org/css-validator/check/referer">CSS</Link> | <Link to="http://www.html5webtemplates.co.uk">HTML5 Web Templates</Link>
