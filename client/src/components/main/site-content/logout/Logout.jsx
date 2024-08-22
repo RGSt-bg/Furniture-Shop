3a46dcd06c678c6dfaf7c48c133e41024a10f860
Rumen Stefanov <rgst.bg@gmail.com>
2024-08-22 09:45:50 +0300

    Added Notification Form to Logout component

--- client/src/components/main/site-content/logout/Logout.jsx
+++ client/src/components/main/site-content/logout/Logout.jsx
@@ -5,37 +5,47 @@ import { AuthContext } from "../../../../App.jsx";
 import { UserIdContext } from "../../Main.jsx";
 
 import { logout } from "../../../../utils/authUtils.js";
+import NotificationForm from "../../../common/NotificationForm.jsx";
 
 export default function Logout() {
   const [message, setMessage] = useState("");
   const {setIsAuth} = useContext(AuthContext);
   const {setUserId} = useContext(UserIdContext);
+  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     const handleLogout = async () => {
-      if (!localStorage.getItem("auth")) {
-        setMessage("You are not logged in!");
-        alert("You are not logged in!");
-        navigate("/auth/login");
-      } else {
+        const response = await logout("/auth/logout");
         try {
-          const response = await logout("/auth/logout");
-          setMessage(response.message);
-          setIsAuth(false);
+          if (response === "Failed to fetch")
+            setMessage("An error occurred in server while logging out!");
+          else
+            setMessage(response.message);
           setUserId('');
-          navigate("/");
-          alert(response.message);
+          setShowModal(!showModal);
+
         } catch (err) {
-          const errorMessage = "An error occurred while logging out!";
-          setMessage(errorMessage);
-          alert(errorMessage);
+          setMessage(message);
+          setShowModal(!showModal);
+          return (showModal && <NotificationForm
+            notices={{ title: "Logout", message}}
+            onClose={handleCloseModal}
+          />);
         }
-      }
     };
 
     handleLogout();
   }, [navigate]);
 
-  return null;
+  const handleCloseModal = () => {
+    setShowModal(!showModal);
+    navigate("/");
+    if (!localStorage.getItem('auth')) setIsAuth(false);
+    if (localStorage.getItem('userId')) localStorage.removeItem('userId');
+  };
+  return (showModal && <NotificationForm
+    notices={{ title: "Logout", message}}
+    onClose={handleCloseModal}
+  />);
 }
